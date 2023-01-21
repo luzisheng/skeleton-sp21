589f39bc31875f878c780d820576d7bb9a7b9b96
luzisheng <18554393368@163.com>
2023-01-21 11:56:01 +0800

    add log

--- proj1ec/tester/TestArrayDequeEC.java
+++ proj1ec/tester/TestArrayDequeEC.java
@@ -12,6 +12,7 @@ public class TestArrayDequeEC {
     public void randomizedTest() {
         ArrayDequeSolution<Integer> correct = new ArrayDequeSolution<>();
         StudentArrayDeque<Integer> broken = new StudentArrayDeque<>();
+        String log = "";
 
         int N = 5000;
         for (int i = 0; i < N; i += 1) {
@@ -20,33 +21,40 @@ public class TestArrayDequeEC {
             if (operationNumber == 0) {
                 // addFirst
                 int randVal = StdRandom.uniform(0, 100);
+                log = log + "addFirst(" + randVal + ")\n";
                 correct.addFirst(randVal);
                 broken.addFirst(randVal);
             } else if (operationNumber == 1) {
                 // addLast
                 int randVal = StdRandom.uniform(0, 100);
+                log = log + "addLast(" + randVal + ")\n";
                 correct.addLast(randVal);
                 broken.addLast(randVal);
             } else if (operationNumber == 2) {
                 // isEmpty
-                assertEquals(correct.isEmpty(), broken.isEmpty());
+                log = log + "isEmpty()\n";
+                assertEquals(log, correct.isEmpty(), broken.isEmpty());
             } else if (operationNumber == 3) {
                 // size
-                assertEquals(correct.size(), broken.size());
+                log = log + "size()\n";
+                assertEquals(log, correct.size(), broken.size());
             } else if (operationNumber == 4 && correct.size() != 0) {
                 // removeFirst
                 Integer firstCorrect = correct.removeFirst();
                 Integer firstBroken = broken.removeFirst();
-                assertEquals(firstCorrect, firstBroken);
+                log = log + "removeFirst()\n";
+                assertEquals(log, firstCorrect, firstBroken);
             } else if (operationNumber == 5 && correct.size() != 0) {
                 // removeLast
+                log = log + "removeLast()\n";
                 Integer lastCorrect = correct.removeLast();
                 Integer lastBroken = broken.removeLast();
-                assertEquals(lastCorrect, lastBroken);
+                assertEquals(log, lastCorrect, lastBroken);
             } else if (operationNumber == 6 && correct.size() != 0) {
                 // get
                 int randVal = StdRandom.uniform(0, broken.size());
-                assertEquals(correct.get(randVal), broken.get(randVal));
+                log = log + "get(" + randVal + ")\n";
+                assertEquals(log, correct.get(randVal), broken.get(randVal));
             }
         }
     }
