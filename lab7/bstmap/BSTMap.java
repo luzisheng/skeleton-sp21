772e301808973f1e8aa30216dc418c04ec5e8dc3
luzisheng <18554393368@163.com>
2023-01-27 23:13:16 +0800

    lab7 start

--- lab7/bstmap/BSTMap.java
+++ lab7/bstmap/BSTMap.java
@@ -1,2 +1,47 @@
-package bstmap;public class BSTMap {
+package bstmap;
+
+import java.util.Iterator;
+import java.util.Set;
+
+public class BSTMap<K extends Comparable<K>, V> implements Map61B<K, V> {
+    // attributes
+
+    // private node
+    private
+    @Override
+    public void clear() {}
+
+    @Override
+    public boolean containsKey(K key) {}
+
+    @Override
+    public V get(K key) {}
+
+    @Override
+    public int size() {}
+
+    @Override
+    public void put(K key, V value) {}
+
+    public void printInOrder() {}
+
+    @Override
+    public Set<K> keySet() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public V remove(K key) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public V remove(K key, V value) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public Iterator<K> iterator() {
+        throw new UnsupportedOperationException();
+    }
 }
